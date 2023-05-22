8ac0a0b23757d2d866020f4db14d3f65ee0ab23e
ChocolateWizard <despotjev@gmail.com>
2023-05-22 17:28:24 +0200

    Added header,footer,pageNotFound components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoaderComponent } from './components/loader/loader.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { SiteLogoComponent } from './components/site-logo/site-logo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    LoaderComponent,
+    PageNotFoundComponent,
+    SiteLogoComponent
   ],
   imports: [
     BrowserModule,
